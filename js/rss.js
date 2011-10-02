55ab0835e14bb5d2e7a7d5d2f9de3bbf1fe635e7
rock506 <rock506@qq.com>
2011-10-02 17:45:44 +0800

    a

--- js/rss.js
+++ js/rss.js
@@ -32,7 +32,7 @@ var RssItemView = Backbone.View.extend({
 });
 //View
 var RssView = Backbone.View.extend({
-    tabName: "div",
+    el: $("body"),
     initialize: function() {
         this.render();
         this.collection.bind("add",this.addItem,this);
